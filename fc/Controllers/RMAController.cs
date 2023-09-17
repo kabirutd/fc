47e32d0dcc6db65a0a8aa12ae7a7446cb0802c47
Mohammad Kabir <mohammadkabir@gmail.com>
2023-09-17 13:08:11 -0500

    Add project files.

---
+++ fc/Controllers/RMAController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fc.Controllers
+{
+    public class RMAController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RMA()
+        {
+            return View();
+        }
+
+        public IActionResult RMAV2()
+        {
+            return View();
+        }
+    }
+}
