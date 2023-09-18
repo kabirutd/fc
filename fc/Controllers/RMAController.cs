9f53aeb11ee4e476bdc0493ee35e97401ad920fe
Mohammad Kabir <mohammadkabir@gmail.com>
2023-09-18 00:07:31 -0500

    09/18/2023

--- fc/Controllers/RMAController.cs
+++ fc/Controllers/RMAController.cs
@@ -4,18 +4,28 @@ namespace fc.Controllers
 {
     public class RMAController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string? custnum)
         {
+            string? cust_num = custnum;
+
+            ViewData["cust_num"] = cust_num;
             return View();
         }
 
-        public IActionResult RMA()
+        public IActionResult RMA(string? custnum)
         {
+            string? cust_num = custnum;
+
+            ViewData["cust_num"] = cust_num;
             return View();
         }
 
-        public IActionResult RMAV2()
+        public IActionResult RMAV2(string? custnum)
         {
+            //get cust_num from
+            string? cust_num = custnum;
+           
+            ViewData["cust_num"] = cust_num;
             return View();
         }
     }
